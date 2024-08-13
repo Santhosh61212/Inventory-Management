00fbf8fb03a82c763601993c6f5e596dbdc47b83
santhosh <santhoshpandiyaraj@gmail.com>
2024-08-13 22:49:04 +0530

    did the code changes for for filter the item and inventory based on id

--- Inventory_Management/src/Inventory/Main.java
+++ Inventory_Management/src/Inventory/Main.java
@@ -17,6 +17,9 @@ public class Main {
 
 			System.out.println(" 4 => getItems");
 			System.out.println(" 5 => getInventory");
+			
+			System.out.println(" 6 => getItemByItemId ");
+			System.out.println(" 7 => getInventoryById ");
 
 			System.out.println("Enter the value : ");
 			
@@ -38,7 +41,19 @@ public class Main {
 				case 5:
 					invService.getInventory();
 					break;
-
+				case 6:
+					System.out.println("please enter the Item id");
+					Long itemId = sc.nextLong();
+					if(itemId>0) {
+						invService.getItemByItemid(itemId);
+					}
+					break;
+				case 7:
+					System.out.println("please enter the Inventory id");
+					Long inventoryId = sc.nextLong();
+					if(inventoryId > 0) {
+						invService.getInventoryById(inventoryId);
+					}
 				}
 			}
 		}
